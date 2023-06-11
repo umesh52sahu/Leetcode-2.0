422d9ae53897fa712ca2bcc742bb452bb818e782
umesh52sahu <88091468+umesh52sahu@users.noreply.github.com>
2023-06-11 08:49:25 +0530

    Time: 7 ms (32.61%), Space: 44.5 MB (24.64%) - LeetHub

--- 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
@@ -11,6 +11,9 @@ class Solution {
         }
         
         if(i>=j) return true;
+        else{
+            
+        }
         
         return palin(a,i,j)||palin(b,i,j);
     }
@@ -25,28 +28,3 @@ class Solution {
         return true;
     }
 }
-// class Solution {
-//     public boolean checkPalindromeFormation(String a, String b) {
-//         return cut(a, b) || cut(b, a);
-//     }
-
-//     private boolean cut(String a, String b) {
-//         int i = 0, j = a.length() - 1;
-//         while (i < j && a.charAt(i) == b.charAt(j)) {
-//             i++; j--;
-//         }
-
-//         return (i >= j)? true :
-//         isPalindrome(a, i, j) || isPalindrome(b, i, j);
-//     }
-
-//     private boolean isPalindrome(String s, int i, int j) {
-//         while (i < j) {
-//             if (s.charAt(i++) != s.charAt(j--)) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// }
