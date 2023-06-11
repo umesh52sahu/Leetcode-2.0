8c3f90e87f1f1f02c88cd3e217469cd92f99bf39
umesh52sahu <88091468+umesh52sahu@users.noreply.github.com>
2023-06-11 08:41:25 +0530

    Time: 7 ms (32.61%), Space: 44.5 MB (18.12%) - LeetHub

---
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean checkPalindromeFormation(String a, String b) {
+      return check(a,b)||check(b,a);
+    }
+    public static boolean check(String a,String b){
+        
+        int i=0;
+        int j=a.length()-1;
+        while(i<j&&a.charAt(i)==b.charAt(j)){
+                i++; j--;
+        }
+        
+        if(i>=j) return true;
+        
+        return palin(a,i,j)||palin(b,i,j);
+    }
+   
+    public static boolean palin(String str,int i,int j){
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j))
+                return false;
+                i++; j--;
+            
+        }
+        return true;
+    }
+}
+// class Solution {
+//     public boolean checkPalindromeFormation(String a, String b) {
+//         return cut(a, b) || cut(b, a);
+//     }
+
+//     private boolean cut(String a, String b) {
+//         int i = 0, j = a.length() - 1;
+//         while (i < j && a.charAt(i) == b.charAt(j)) {
+//             i++; j--;
+//         }
+
+//         return (i >= j)? true :
+//         isPalindrome(a, i, j) || isPalindrome(b, i, j);
+//     }
+
+//     private boolean isPalindrome(String s, int i, int j) {
+//         while (i < j) {
+//             if (s.charAt(i++) != s.charAt(j--)) {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
