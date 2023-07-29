f2343678bdb4315a0f7586cab35ba4aec856a2d5
umesh52sahu <88091468+umesh52sahu@users.noreply.github.com>
2023-07-29 12:37:19 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (86.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -9,22 +9,40 @@
 //         return ans1+ans2;
 //     }
 // }
-class Solution {
-    public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        
-        dp[0]=0;
-        dp[1]=1;
-        if(n>=2){
-        dp[2]=2;
-        }
-        for(int i=3;i<=n;i++){
-            int cs1=dp[i-1];
-            int cs2=dp[i-2];
-            dp[i] =cs1+cs2;
-        }
-        return dp[n];
-        
+
+class Solution{
+    public int climbStairs(int n){
+        Integer[] dp = new Integer[n+1];
+        int ans = climbStairs1(n, dp);
+        return ans;
     }
-    
+    public int climbStairs1(int n, Integer[] dp){
+        if(n==0){
+            return 1;
+        }
+        if(n<0) return 0;
+        if(dp[n]!=null){
+            return dp[n]; 
+        }
+        int ans1= climbStairs1(n-1, dp);
+        int ans2= climbStairs1(n-2, dp);
+        dp[n]=ans1+ans2;
+        return ans1+ans2;
+    }
 }
+// class Solution {
+//     public int climbStairs(int n) {
+//         int[] dp = new int[n+1];
+//         dp[0]=0;
+//         dp[1]=1;
+//         if(n>=2){
+//         dp[2]=2;
+//         }
+//         for(int i=3;i<=n;i++){
+//             int cs1=dp[i-1];
+//             int cs2=dp[i-2];
+//             dp[i] =cs1+cs2;
+//         }
+//         return dp[n];
+//     } 
+// }
